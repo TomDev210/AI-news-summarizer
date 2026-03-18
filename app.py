7393fc1260e2c4cab277c51a8ba9189c0073516c
Rajalakshmi G <165365222+TomDev210@users.noreply.github.com>
2026-03-18 11:19:33 +0530

    Modify app.run to set host and port dynamically
    Update app.run to use environment variable for port

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import validators
 import requests
 from googletrans import Translator
 from nltk.tokenize import sent_tokenize
+import os
 
 nltk.download('punkt')
 
@@ -88,4 +89,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
